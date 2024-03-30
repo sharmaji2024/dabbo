52246bc97746cd60776fb40f5db6ddb073d90008
sharmaji2024 <165476345+sharmaji2024@users.noreply.github.com>
2024-03-30 17:15:41 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,129 @@
+
+  $('.projects').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+
+  $('#owl-carousel-second').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+    items: 4,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      }
+    },
+  })
+
+  $('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      }
+    },
+  })
+
+
+  $('.OurTestimonails').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
+  
+
+  $('.team-slider').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+
+  $('.post-owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 2
+      }
+    }
+  })
